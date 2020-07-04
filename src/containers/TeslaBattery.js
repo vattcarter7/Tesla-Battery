0697e0c4dc30df0498ea9b81988f912a05d1d3d8
vattcarter7 <vattcarter@gmail.com>
2020-07-04 14:56:50 +0700

    implement initial state of the main app

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -3,6 +3,19 @@ import './TeslaBattery.scss';
 import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
 
 class TeslaBattery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      carstats: [],
+      config: {
+        speed: 55,
+        temperature: 20,
+        climate: true,
+        wheels: 19
+      }
+    };
+  }
+
   render() {
     return (
       <form className='tesla-battery'>
