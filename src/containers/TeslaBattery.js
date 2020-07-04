7740523807dc588cd0e897702e4239732fd299c2
vattcarter7 <vattcarter@gmail.com>
2020-07-04 13:55:39 +0700

    added header

---
+++ src/containers/TeslaBattery.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TeslaBattery.scss';
+
+class TeslaBattery extends React.Component {
+  render() {
+    return (
+      <form className='tesla-battery'>
+        <h1>Range Per Charge</h1>
+      </form>
+    );
+  }
+}
+export default TeslaBattery;
