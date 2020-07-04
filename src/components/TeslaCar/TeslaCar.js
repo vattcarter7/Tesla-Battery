3678c3aa29b5e16f7eeac3f67d09cb42c4a5eb18
vattcarter7 <vattcarter@gmail.com>
2020-07-04 14:29:13 +0700

    added TeslaCar component

---
+++ src/components/TeslaCar/TeslaCar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './TeslaCar.scss';
+
+const TeslaCar = (props) => (
+  <div className='tesla-car'>
+    <div className='tesla-wheels'>
+      <div
+        className={`tesla-wheel tesla-wheel--front tesla-wheel--${props.wheelsize}`}
+      ></div>
+      <div
+        className={`tesla-wheel tesla-wheel--rear tesla-wheel--${props.wheelsize}`}
+      ></div>
+    </div>
+  </div>
+);
+
+TeslaCar.propTypes = {
+  wheelsize: PropTypes.number
+};
+
+export default TeslaCar;
