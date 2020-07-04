e31d1bcd05d5bd479ac90d13ec645d805b14c11d
vattcarter7 <vattcarter@gmail.com>
2020-07-04 15:58:55 +0700

    implemented TeslaBattery calculateStats and statsUpdate functions

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -3,6 +3,7 @@ import './TeslaBattery.scss';
 import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
 import TeslaCar from '../components/TeslaCar/TeslaCar';
 import TeslaStats from '../components/TeslaStats/TeslaStats';
+import { getModelData } from '../services/BatteryService';
 
 class TeslaBattery extends React.Component {
   constructor(props) {
@@ -18,6 +19,34 @@ class TeslaBattery extends React.Component {
     };
   }
 
+  calculateStats = (models, value) => {
+    const dataModels = getModelData();
+    return models.map((model) => {
+      // takes out required values and create references to them
+      const { speed, temperature, climate, wheels } = value;
+      const miles =
+        dataModels[model][wheels][climate ? 'on' : 'off'].speed[speed][
+          temperature
+        ];
+      return {
+        model,
+        miles
+      };
+    });
+  };
+
+  statsUpdate = () => {
+    const carModels = ['60', '60D', '75', '75D', '90D', 'P100D'];
+    // Fetch model info from BatteryService and calculate then update state
+    this.setState({
+      carstats: this.calculateStats(carModels, this.state.config)
+    });
+  };
+
+  componentDidMount() {
+    this.statsUpdate();
+  }
+
   render() {
     const { config, carstats } = this.state;
     return (
@@ -30,4 +59,5 @@ class TeslaBattery extends React.Component {
     );
   }
 }
+
 export default TeslaBattery;
